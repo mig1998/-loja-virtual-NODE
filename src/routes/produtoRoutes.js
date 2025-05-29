3f7b12720927d2775c5518b87b5830f6a50bca10
mig1998 <miguelaras@hotmail.com>
2025-05-28 21:28:53 -0300

    ok

--- src/routes/produtoRoutes.js
+++ src/routes/produtoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const produtoController = require('../controllers/produtoController');
 
-
+//
 
 // Definir rotas
 router.get('/', produtoController.getAllProdutos);
